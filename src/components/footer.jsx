1030d855764dc22312f1562aa469dd8138b2bb87
zhuxiaolong <1743769514@qq.com>
2024-03-20 16:36:24 +0800

    init-app

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,12 @@
+/*
+ * @Author: 朱小龙
+ * @Date: 2024-03-20 16:33:53
+ * @LastEditTime: 2024-03-20 16:34:00
+ * @LastEditors: 朱小龙
+ * @Description: 
+ * @FilePath: \my-app\src\components\footer.jsx
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
 import React, { Component } from 'react'
 
 export default class footer extends Component {
